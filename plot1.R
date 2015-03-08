da93b5d1476ad28092d003c447c370da5a2177ba
Noskirec <caleb.erikson@gmail.com>
2015-03-08 17:44:29 -0400

    Updated plot1 with comments. Tested margin settings, but decided not to include.

--- plot1.R
+++ plot1.R
@@ -4,16 +4,22 @@
 
 plot1 <- function(){
   data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+  # Reformat dates
   data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
   
-  data.subset <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
+  # Grab appropriate subset of data
+  data_subset <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
   
-  for(i in c(3:9)) {data.subset[,i] <- as.numeric(as.character(data.subset[,i]))}
+  for(i in c(3:9)) {data_subset[,i] <- as.numeric(as.character(data_subset[,i]))}
   
   png(filename = "plot1.png", width = 480, height = 480, units = "px", bg = "white")
   
-  par(mar = c(6,6,5,4))
+  # Testing margin settings
+  #par(mar = c(6,6,5,4))
   
-  hist(data.subset$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+  hist(data_subset$Global_active_power, col = "red", main = "Global Active Power", 
+       xlab = "Global Active Power (kilowatts)")
+  
+  # Turn off graphics device
   dev.off()
 }
